8510d0723d6de0fdc66d9e178f1625d9848b972c
Patrick <privat@skyraptor.eu>
2021-12-06 05:45:20 +0100

    Create install-service.sh

---
+++ install-service.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Create a systemd service that autostarts & manages a docker-compose instance in the current directory
+# by Uli Köhler - https://techoverflow.net
+echo "Creating systemd service as /etc/systemd/system/redm.service"
+# Create systemd service file
+sudo cat >/etc/systemd/system/$SERVICENAME.service <<EOF
+[Unit]
+Description=RedM
+Requires=docker.service
+After=docker.service
+[Service]
+Restart=always
+User=root
+Group=docker
+WorkingDirectory=$(pwd)
+# Shutdown container (if running) when unit is started
+ExecStartPre=$(which docker-compose) -f docker-compose.yml down
+# Start container when unit is started
+ExecStart=$(which docker-compose) -f docker-compose.yml up
+# Stop container when unit is stopped
+ExecStop=$(which docker-compose) -f docker-compose.yml down
+[Install]
+WantedBy=multi-user.target
+EOF
+echo "Enabling & starting redm"
+# Autostart systemd service
+sudo systemctl enable redm.service
+# Start systemd service now
+sudo systemctl start redm.service
