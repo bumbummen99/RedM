548455192177c4a054d6ad8cfec19f2c0caeab61
Patrick <privat@skyraptor.eu>
2021-12-08 00:18:07 +0100

    Update extract-sql.sh

--- maintenance/extract-sql.sh
+++ maintenance/extract-sql.sh
@@ -3,9 +3,9 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 # Load .env or default
 if [ -f "$SCRIPT_DIR/.env" ]; then
-  export $(cat $SCRIPT_DIR/.env | sed 's/#.*//g' | xargs)
+  export $(cat $SCRIPT_DIR/../.env | sed 's/#.*//g' | xargs)
 else
-  export $(cat $SCRIPT_DIR/.env.example | sed 's/#.*//g' | xargs)
+  export $(cat $SCRIPT_DIR/../.env.example | sed 's/#.*//g' | xargs)
 fi
 
 function hasDatabse() {
@@ -20,17 +20,17 @@ function hasDatabse() {
 echo "Default database is $MYSQL_DATABASE"
 
 # Remove old sql files
-rm $SCRIPT_DIR/sql/*.sql
+rm $SCRIPT_DIR/../sql/*.sql
 
 # Write sql to create the default database
-echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\`;" > $SCRIPT_DIR/sql/0-create-default-database.sql
-echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';" >> $SCRIPT_DIR/sql/0-create-default-database.sql
-echo "GRANT ALL PRIVILEGES ON essentialmode.* TO '$MYSQL_USER'@'%';" >> $SCRIPT_DIR/sql/0-create-default-database.sql
+echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\`;" > $SCRIPT_DIR/../sql/0-create-default-database.sql
+echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';" >> $SCRIPT_DIR/../sql/0-create-default-database.sql
+echo "GRANT ALL PRIVILEGES ON essentialmode.* TO '$MYSQL_USER'@'%';" >> $SCRIPT_DIR/../sql/0-create-default-database.sql
 
 echo "Added default databse .sql"
 
 # Create a new tmp dir
-TMP=`mktemp -d -p "$SCRIPT_DIR"`
+TMP=`mktemp -d -p "$SCRIPT_DIR/.."`
 
 echo "Create TMP directory at $TMP"
 
@@ -54,7 +54,7 @@ for SQL in "$TMP"/*.sql; do
   fi
   
   # Copy the corrected SQL to the sql folder so it can be imported to initdb
-  cp "$SQL" "$SCRIPT_DIR/sql/1-$(basename -- $SQL)"
+  cp "$SQL" "$SCRIPT_DIR/../sql/1-$(basename -- $SQL)"
 done
 
 echo "All .sql files processed!"
