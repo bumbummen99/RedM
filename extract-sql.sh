884513f934462f3d561c915d505e082da58d036e
Patrick <privat@skyraptor.eu>
2021-12-07 16:52:50 +0100

    Update extract-sql.sh

--- extract-sql.sh
+++ extract-sql.sh
@@ -13,6 +13,12 @@ function hasDatabse() {
 # Remove old sql files
 rm $SCRIPT_DIR/sql/*.sql
 
+# Write sql to create the default database
+echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\`;" > $SCRIPT_DIR/sql/0-create-default-database.sql
+echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';" >> $SCRIPT_DIR/sql/0-create-default-database.sql
+
+echo "Added default databse .sql"
+
 # Load .env or default
 if [ -f "$SCRIPT_DIR/.env" ]; then
   export $(cat $SCRIPT_DIR/.env | sed 's/#.*//g' | xargs)
@@ -21,6 +27,25 @@ else
 fi
 
 echo "Default database is $MYSQL_DATABASE"
+
+# Special: Check if redem_roleplay exists and extract db
+if [ -d "$SCRIPT_DIR/resources/\[redemrp\]/redem_roleplay" ]; then
+  echo "Extracting redem_roleplay sql..."
+  
+  for SQL in "$TMP"/resources/\[redemrp\]/redem_roleplay/server/sql/*.sql; do
+    # Skip files that do not exist
+    if ! [ -f "$SQL" ]; then
+      continue;
+    fi
+
+    echo "Processing file $SQL"
+
+    # Copy the corrected SQL to the sql folder so it can be imported to initdb
+    cp "$SQL" "$SCRIPT_DIR/sql/1-$(basename -- $SQL)"
+  done
+  
+  echo "Done extracting redem_roleplay sql!"
+fi
 
 # Create a new tmp dir
 TMP=`mktemp -d -p "$SCRIPT_DIR"`
@@ -47,7 +72,7 @@ for SQL in "$TMP"/*.sql; do
   fi
   
   # Copy the corrected SQL to the sql folder so it can be imported to initdb
-  cp "$SQL" "$SCRIPT_DIR/sql/1-$(basename -- $SQL)"
+  cp "$SQL" "$SCRIPT_DIR/sql/2-$(basename -- $SQL)"
 done
 
 echo "All .sql files processed!"
@@ -56,11 +81,5 @@ echo "All .sql files processed!"
 rm -rf "$TMP"
 
 echo "Removed TMP directoy."
-
-# Write sql to create the default database
-#echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\`;" > $SCRIPT_DIR/sql/0-create-default-database.sql
-#echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';" >> $SCRIPT_DIR/sql/0-create-default-database.sql
-
-#echo "Added default databse .sql"
 
 echo "Done! You can import the sql now."
